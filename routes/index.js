6664cdcd09fd0aa8382460cadbd4c736f02a2215
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-23 15:09:28 -0600

    ADD: hasta aqui gran avance

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const RegisterController = require("../controllers/SismosController");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let sismos = RegisterController.getAll;
-  res.render('index', { title: 'Sismos', h1: 'REGISTROS', sismos });
+  res.render('index', { title: 'Sismos', h1: 'REGISTROS' });
+  console.log(sismos);
+  
 });
 
 module.exports = router;
