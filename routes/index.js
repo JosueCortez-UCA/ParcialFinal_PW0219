98ad518b9e4665d7ee896f4dda70255b1761d8e2
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-23 14:58:07 -0600

    ADD: intento 1 de imprimir registros

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const RegisterController = require("../controllers/SismosController");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sismos' });
+  let sismos = RegisterController.getAll;
+  res.render('index', { title: 'Sismos', h1: 'REGISTROS', sismos });
 });
 
 module.exports = router;
