98fce5e08bddebe46adef4965072e182ea229c74
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-23 16:29:15 -0600

    ADD: pequenos cambios

--- models/Sismo.js
+++ models/Sismo.js
@@ -1,12 +1,15 @@
-const Mongoose= require ("mongoose");
+const Mongoose= require ("mongoose"),
+    Schema = Mongoose.Schema;
 
-const sismoSchema = Mongoose.Schema({
+const sismoSchema = new Schema({
     titulo: String,
     magnitud: Number,
     epicentro: String,
     latitud: Number,
     longitud: Number,
     replicas: Number,
+}, {
+    timestamps: true
 });
 
 module.exports = Mongoose.model("Sismo", sismoSchema);
